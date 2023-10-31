6dca2a40dc971498bec7090c0b221ee4e1475449
emmanuel mhango <emmanuellmhango@gmail.com>
2023-10-31 12:59:15 +0200

    Updated facility controller to include serach and filter functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/facilities_search', to: 'facilities#facilities_search'
+      get '/facilities_filter', to: 'facilities#facilities_filter'
       resources :facilities
       resources :users
     end
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
